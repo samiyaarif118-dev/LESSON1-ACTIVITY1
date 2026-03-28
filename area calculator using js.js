369858469ced718e929383f88464f733f0a8c09b
samiyaarif118-dev <samiyaarif118@gmail.com>
2026-03-28 13:17:52 +0100

    Add files via upload

---
+++ area calculator using js.js
@@ -0,0 +1,24 @@
+var choice = prompt("Welcome to area calculator. \n please enter your choice.\n1. Area of Rectangle. \n2.Area of Triangle \n3.Area of Circle \n4.Area of Parellelogram");
+if (choice == '1') {
+    var l = prompt('Enter the Length')
+    var b = prompt('Enter the Breadth')
+    var result = Number(l) * Number(b)
+    alert('The Area is ' + result)
+}
+if (choice == '2') {
+    var h = prompt('Enter the height')
+    var b = prompt('Enter the base')
+    var result = Number(h) * Number(b) / 2
+    alert('The Area is ' + result)
+}
+if (choice == '3') {
+   var r = prompt('Enter the radius')
+   var result = 3.14 * Number(r) * Number(r)
+   alert('The Area is ' + result) 
+}
+if (choice == '4') {
+    var h = prompt('Enter the height')
+    var cb = prompt('Enter the corresponding base')
+    var result = Number(h) * Number(cb)
+    alert('The Area is ' + result)
+}
