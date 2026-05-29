276de3b4fcc1616db19f019c9fa4c05a0ca9a544
samiyaarif118-dev <samiyaarif118@gmail.com>
2026-05-29 19:45:18 +0200

    Add files via upload

---
+++ introduction.py
@@ -0,0 +1,15 @@
+print("Hello, World!")
+print("Welcome to my first python program")
+print(12)
+print("We are learning about ", end="python")
+print("\nMy name is Samiya. \nI am 13 years old. \nI am in grade 7.")
+names = ("Samiya", "Haniya", "Maham")
+print(names)
+print(names, "are friends")
+number = input("Enter your birthday: ")
+import keyword
+print(keyword.kwlist)
+
+      
+      
+      
