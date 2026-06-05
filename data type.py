2fdbf610e6d36decb3382f75514900cb1368eb6c
samiyaarif118-dev <samiyaarif118@gmail.com>
2026-06-05 19:49:02 +0200

    Add files via upload

---
+++ data type.py
@@ -0,0 +1,33 @@
+integer = 13
+print(integer)
+print(type(integer))
+
+decimal = 2.3
+print(decimal)
+print(type(decimal))
+
+string = "Hi Everyone"
+print(string)
+print(type(string))
+
+boolean = False
+print(boolean)
+print(type(boolean))
+
+height = 5.4
+height2 = int(height)
+print(height2)
+print(type(height2))
+
+a = 30
+b = float(a)
+print(b)
+print(type(b))
+
+name = input("Enter Your name: ")
+print("Hi, " + name + "! Welcome to python programming.")
+
+str = "Good"
+str1 = "Night"
+print(str + " " + str1)
+print(str[1:3])
