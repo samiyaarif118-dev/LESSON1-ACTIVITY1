09961a4983982289a04e88834830949d61b14bc3
samiyaarif118-dev <samiyaarif118@gmail.com>
2026-06-06 18:54:04 +0200

    Add files via upload

---
+++ congratulations.py
@@ -0,0 +1,9 @@
+name = input("Enter Your name: ")
+greeting = "Hi, " + name + "! here is a message for you."
+print(greeting)
+message = "Congratulations! You got the gold medal successfully. Your hard work and dedication have paid off."
+print(message)
+print(type(message))
+upper_message = message.upper()
+print(upper_message)
+
