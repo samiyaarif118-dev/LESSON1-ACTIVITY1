7e30f08189e9972c5b72e90791f7a0f2f069a3dd
samiyaarif118-dev <samiyaarif118@gmail.com>
2026-06-14 13:29:56 +0200

    Add files via upload

---
+++ summertime.py
@@ -0,0 +1,13 @@
+#if statement 
+temperature = int(input("enter the temperature: "))
+if temperature < 40:
+    print("it's a nice day")
+
+
+#if_else statement
+temperature = int(input("enter the temperature: "))
+if temperature < 15:
+    print("it's a cold day")
+else:
+    print("it's a rainy day")
+
