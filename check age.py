19904ba0267778601866cd8411007afe93383272
samiyaarif118-dev <samiyaarif118@gmail.com>
2026-06-19 23:39:56 +0200

    Add files via upload

---
+++ check age.py
@@ -0,0 +1,9 @@
+age = int(input("Enter your age: "))
+if age >= 10:
+    if age >=20:
+        print("you are too old to join the party")
+    else:
+        print("you are allowed to join the party")
+else:
+    print("you are too young to join the party")
+
